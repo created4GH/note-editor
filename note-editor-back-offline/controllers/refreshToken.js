e5b0f7f9fc68d0085a0abfbaa92a11a8e9038770
created4GitHub <90993771+created4GitHub@users.noreply.github.com>
2022-11-17 23:30:48 +0200

    Delete note-editor-back-offline directory

--- note-editor-back-offline/controllers/refreshToken.js
+++
@@ -1,9 +0,0 @@
-const setCookies = require('../config/setCookies');
-
-const handleRefreshToken = (req, res) => {
-        const {username, password} = req.user;
-        setCookies(res, username, password);
-        return res.sendStatus(200);
-}
-
-module.exports = handleRefreshToken;
